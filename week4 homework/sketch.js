e198003af7f27a1b1a2553f01d9d20627f9585b1
mengzhenxiao <mengzhenxiao@users.noreply.github.com>
2017-10-01 00:17:04 -0400

    Add Comments

--- week4 homework/sketch.js
+++ week4 homework/sketch.js
@@ -1,32 +1,33 @@
 var star = [];
 var z;
 
-
 function setup() {
   createCanvas(800, 600)
   for (var i = 0; i < 800; i++) {
+    //make a star array, and the array is a star function.
     star[i] = new Star();
   }
 }
 
 
-
 function draw() {
   background(0);
   translate(width / 2, height / 2);
+    
+  //loop the star show function
   for (var i = 0; i < star.length; i++) {
     star[i].show();
   }
-
 }
 
-
+//the whole star funtion
 function Star() {
+  //make main variables
   this.x = random(-width, width);
   this.y = random(-height, height);
   this.z = random(width);
 
-
+  //this funtion draw the stars and make them move
   this.show = function() {
     //make the speed change with the mouseX
     this.speed = map(mouseY, 0, height, 30, 5);
@@ -46,6 +47,5 @@ function Star() {
     this.sy = map(this.y / this.z, 0, 1, 0, height);
     this.r = map(this.z, 0, width, 8, 0);
     ellipse(this.sx, this.sy, this.r, this.r);
-
   }
 }
