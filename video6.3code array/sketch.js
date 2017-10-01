5e718e610d9bc009d62c2565d3bf76aa18d79761
mengzhenxiao <mengzhenxiao@users.noreply.github.com>
2017-09-30 22:12:49 -0400

    6.4: The Constructor Function in JavaScript

--- video6.3code array/sketch.js
+++ video6.3code array/sketch.js
@@ -3,20 +3,7 @@ var bubbles = [];
 function setup(){
     createCanvas(600,400);
     for (var i = 0; i< 10; i++){
-        bubbles[i] = {
-            x: random(0,width),
-            y: random(0,height),
-            display: function(){
-                stroke (255);
-                noFill();
-                ellipse(this.x, this.y, 24,24);
-            },
-            move: function(){
-                this.x = this.x+random(-1,1);
-                this.y = this.y+random(-1,1);
-            }
-           
-         }
+        bubbles[i] = new Bubble(); 
         }
     }
 
@@ -28,3 +15,34 @@ function draw(){
         bubbles[i].display();
     }
 }
+
+
+function Bubble(){
+    this.x=random(0,width);
+    this.y=random(0,height);
+    this.display= function(){
+        stroke (255);
+        noFill();
+        ellipse(this.x, this.y, 24,24);
+    }
+    this.move=function(){
+        this.x = this.x+random(-1,1);
+        this.y = this.y+random(-1,1);
+    }
+}
+
+
+/*
+x: random(0,width),
+            y: random(0,height),
+            display: function(){
+                stroke (255);
+                noFill();
+                ellipse(this.x, this.y, 24,24);
+            },
+            move: function(){
+                this.x = this.x+random(-1,1);
+                this.y = this.y+random(-1,1);
+            }
+            
+            */
