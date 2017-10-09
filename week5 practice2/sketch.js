e9ab1ec48e7447b646d162673ac8a640e4b03bca
mengzhenxiao <mengzhenxiao@users.noreply.github.com>
2017-10-08 22:40:55 -0400

    click a single object and make it move and change color

--- week5 practice2/sketch.js
+++ week5 practice2/sketch.js
@@ -1,43 +1,54 @@
-let bubble[];
+let bubble = [];
 
-function setup(){
-    createCanvas(400,400);    
-    for(let i=0; i<10; i++){
-        bubble[i]=new Ball();
-    }
-    
-    
-    
+function setup() {
+  createCanvas(400, 400);
+  for (let i = 0; i < 10; i++) {
+    bubble[i] = new Bubble();
+  }
 }
 
-
-
-function draw (){
-    background(255);
+function draw() {
+  background(0);
+  for (let i = 0; i < bubble.length; i++) {
     bubble[i].show();
     bubble[i].move();
-    
+  }
 }
 
+function mousePressed() {
+  for (let i = 0; i < bubble.length; i++) {
+    bubble[i].mouseClicked();
+  }
+}
 
-class Ball{
-    constructior(){
-        this.x=random(0,width);
-        this.y=random(0,height);
-        this.r=30;
-        this.col=(200,100)
+class Bubble {
+  constructor() {
+    this.x = random(0, width);
+    this.y = random(0, height);
+    this.r = 30;
+    this.col = (200);
+    this.on = false;
+    //this.d=dist(mouseX,mouseY,this.x,this.y);
+  }
+
+  show() {
+    noStroke();
+    fill(this.col);
+    ellipse(this.x, this.y, this.r, this.r);
+  }
+
+  move() {
+    if (this.on) {
+      this.x = this.x + random(-1, 1);
+      this.y = this.y + random(-1, 1);
     }
-    
-    
-    show(){
-        fill(this.col);
-        ellipse(this.x,this.y,this.r,this.r);
-        
+  }
+
+  mouseClicked() {
+    let d = dist(mouseX, mouseY, this.x, this.y);
+    if (d < 15) {
+      this.on = !this.on;
+      this.col = color(200, 100, 50, 200);
     }
-    
-    move(){
-        this.x=this.x+random(-1,1);
-        this.y=this.y+random(-1,1);    
-    }
-    
+  }
 }
