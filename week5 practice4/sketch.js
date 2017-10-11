905e4927c4f18cdee6c5f05e5679de76117f47e0
mengzhenxiao <mengzhenxiao@users.noreply.github.com>
2017-10-11 00:04:06 -0400

    done half

--- week5 practice4/sketch.js
+++ week5 practice4/sketch.js
@@ -7,52 +7,50 @@ function setup() {
 
 function draw() {
   background(0);
-        for (let i = 0; i < 8; i++) {
-        for (var o = 60; o <= 520; o = o + 60) {
-            button[i] = new Button(i*60+60,o,30);
-            button[i].show();
-            button[i].mouseClicked();
-            
-        } 
-            
-      }
+  for (let i = 0; i < 8; i++) {
+    for (var o = 60; o <= 520; o = o + 60) {
+      button[i] = new Button(i * 60 + 60, o, 30);
+        
+      if(mouseIsPressed){button[i].mouseIn();}//This is not the ideal function I want.
+        
+      button[i].show();
+    }
+  }
 }
 
-//function mousePressed() {
-  //for (let i = 0; i < button.length; i++) {
-   // button[i].mouseClicked();
-  //}
-//}
-      
+//Why it can't work in this way. :(
+/*function mousePressed() {
+for (let i = 0; i < button.length; i++) {
+for (var o = 60; o <= 520; o = o + 60) {
+button[i].mouseIn();
+}
+}}*/
+
 
 class Button {
-  constructor(x,y,h) {
+  constructor(x, y, h) {
     this.x = x;
     this.y = y;
     this.h = h;
-      this.colf=color(131,194,213);
-      this.colb=color(93,143,158);
+    this.colf = color(131, 194, 213);
+    this.colb = color(93, 143, 158);
   }
-    
+
   show() {
-      rectMode(CENTER);
+    rectMode(CENTER);
     fill(this.colb);
     noStroke();
     rect(this.x, this.y, this.h, this.h);
     fill(this.colf);
-    rect(this.x, this.y, this.h-8, this.h-8);
-   
+    rect(this.x, this.y, this.h - 8, this.h - 8);
   }
- 
 
-     
-   
-      mouseClicked() {
-    let d = dist(mouseX, mouseY, this.x, this.y);
+
+  mouseIn() {
+    let d = dist(this.x, this.y, mouseX, mouseY);
     if (d < 15) {
-      this.col = color(200, 100, 50, 200);
+      this.colf = color(155,171,122);
+        this.colb=color(220, 246, 165);
     }
-     
-     
- }
-    
+  }
+} 
