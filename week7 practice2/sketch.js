ef62453f9bfb03e2009881739516db1250c3acc3
mengzhenxiao <mengzhenxiao@users.noreply.github.com>
2017-10-21 15:56:48 -0400

    make a weather diagram

---
+++ week7 practice2/sketch.js
@@ -0,0 +1,70 @@
+var weatherS;
+var weatherN;
+
+function setup() {
+  createCanvas(400, 400);
+  loadJSON('https://api.openweathermap.org/data/2.5/forecast/daily?q=Shanghai&mode=json&units=metric&cnt=7&APPID=001b0f58045147663b1ea518d34d88b4', gotData1);
+  loadJSON('https://api.openweathermap.org/data/2.5/forecast/daily?q=Newyork&mode=json&units=metric&cnt=7&APPID=001b0f58045147663b1ea518d34d88b4', gotData2);
+  rectMode(CENTER);
+  textAlign(CENTER);
+}
+
+function gotData1(data) {
+  weatherS = data;
+}
+
+function gotData2(data) {
+  weatherN = data;
+}
+
+function draw() {
+  background(220);
+  lines();
+  data();
+}
+
+
+function data() {
+  if (weatherS) {
+    for (var i = 0; i < weatherS.list.length; i++) {
+      fill(0, 0, 200);
+      noStroke();
+      ellipse(60 + i * 50, 300 - (weatherS.list[i].temp.day * 5), 10, 10);
+      rect(280, 50, 30, 10);
+      text(weatherS.city.name, 330, 55);
+    }
+    for (var h = 0; h < weatherS.list.length - 1; h++) {
+      stroke(0, 0, 200);
+      line(60 + h * 50, 300 - (weatherS.list[h].temp.day * 5), 60 + (h + 1) * 50, 300 - (weatherS.list[h + 1].temp.day * 5));
+    }
+  }
+
+  if (weatherN) {
+    for (var j = 0; j < weatherN.list.length; j++) {
+      fill(0, 200, 0);
+      noStroke();
+      ellipse(60 + j * 50, 300 - (weatherN.list[j].temp.day * 5), 10, 10);
+      rect(280, 70, 30, 10);
+      text(weatherN.city.name, 330, 75);
+    }
+    for (var k = 0; k < weatherN.list.length - 1; k++) {
+      stroke(0, 200, 0);
+      line(60 + k * 50, 300 - (weatherN.list[k].temp.day * 5), 60 + (k + 1) * 50, 300 - (weatherN.list[k + 1].temp.day * 5));
+    }
+  }
+  
+}
+
+
+function lines() {
+  stroke(126);
+  fill(0);
+  line(40, 300, 380, 300);
+  line(40, 300, 40, 100);
+  for(var g=0;g<8;g++){
+  text(g*5, 30, 300-g*25);
+  }
+  text("Today", 60, 320);
+  text("7 days before", 340, 320);
+  text("Celsius",40,80);
+}
