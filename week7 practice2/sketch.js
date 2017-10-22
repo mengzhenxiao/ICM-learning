5b0fd95d7df13a007720cc64f834d28d8ad0df5a
mengzhenxiao <mengzhenxiao@users.noreply.github.com>
2017-10-21 20:09:51 -0400

    make city search

--- week7 practice2/sketch.js
+++ week7 practice2/sketch.js
@@ -1,12 +1,43 @@
 var weatherS;
 var weatherN;
+var api = 'https://api.openweathermap.org/data/2.5/forecast/daily?q=';
+var mode = '&mode=json';
+var units = '&units=metric';
+var cnt = '&cnt=7';
+var appid = '&APPID=001b0f58045147663b1ea518d34d88b4';
+
+
+var input1;
+var input2;
+
 
 function setup() {
   createCanvas(400, 400);
-  loadJSON('https://api.openweathermap.org/data/2.5/forecast/daily?q=Shanghai&mode=json&units=metric&cnt=7&APPID=001b0f58045147663b1ea518d34d88b4', gotData1);
-  loadJSON('https://api.openweathermap.org/data/2.5/forecast/daily?q=Newyork&mode=json&units=metric&cnt=7&APPID=001b0f58045147663b1ea518d34d88b4', gotData2);
+  var button1 = select('#submit1');
+  var button2 = select('#submit2');
+
+  button1.mousePressed(weatherAsk1);
+  button2.mousePressed(weatherAsk2);
+  input1 = select('#city1');
+  input2 = select('#city2');
+
   rectMode(CENTER);
   textAlign(CENTER);
+}
+
+function weatherAsk1() {
+  var url1 = api + input1.value() + mode + units + cnt + appid;
+  //var url2=api+input2.value()+mode+units+cnt+appid;
+  loadJSON(url1, gotData1);
+  //loadJSON(url2, gotData2);
+}
+
+
+function weatherAsk2() {
+  //var url1=api+input1.value()+mode+units+cnt+appid;
+  var url2 = api + input2.value() + mode + units + cnt + appid;
+  //loadJSON(url1, gotData1);
+  loadJSON(url2, gotData2);
 }
 
 function gotData1(data) {
@@ -52,7 +83,7 @@ function data() {
       line(60 + k * 50, 300 - (weatherN.list[k].temp.day * 5), 60 + (k + 1) * 50, 300 - (weatherN.list[k + 1].temp.day * 5));
     }
   }
-  
+
 }
 
 
@@ -61,10 +92,10 @@ function lines() {
   fill(0);
   line(40, 300, 380, 300);
   line(40, 300, 40, 100);
-  for(var g=0;g<8;g++){
-  text(g*5, 30, 300-g*25);
+  for (var g = 0; g < 8; g++) {
+    text(g * 5, 30, 300 - g * 25);
   }
   text("Today", 60, 320);
   text("7 days before", 340, 320);
-  text("Celsius",40,80);
+  text("Celsius", 40, 80);
 }
